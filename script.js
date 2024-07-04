8cff33c5114f3b97d460ec3d95bb0467544e29ca
Sofia <sofia.cronstrom@gmail.com>
2024-07-04 14:38:27 +0200

    stege

---
+++ script.js
@@ -0,0 +1,18 @@
+////STEGE
+
+// let goal = 1;
+// let numOfThrows = 0;
+
+// while (goal < 7){
+//     let dice = Math.floor(Math.random() * 6 + 1);
+//     numOfThrows++;
+//     console.log(dice)
+//     if (dice === goal){
+//         goal++;
+//         console.log('Wohoo');
+//     }
+// }
+// console.log(`Grattis du fick stege på ${numOfThrows} kast`)
+
+////KNOCK OUT
+let knockOutNr = parseInt(promt('Choose a knock'))
